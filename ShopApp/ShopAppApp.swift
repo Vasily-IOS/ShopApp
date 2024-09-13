8a5429a80799404b3396687a25e06ea7d9d08c03
Vasily-IOS <vasyaburton91@gmail.com>
2024-09-13 14:03:57 +0300

    first commit

---
+++ ShopApp/ShopAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShopAppApp.swift
+//  ShopApp
+//
+//  Created by Василий on 13.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct ShopAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
